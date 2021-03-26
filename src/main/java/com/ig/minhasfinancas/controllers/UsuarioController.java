97467095aa5c1b636ad99d98afbf0cd6f36c0050
Ian Gerolamo <iangerolamo@gmail.com>
2021-03-26 11:35:05 -0300

    Implementação do POST de Usuários

--- src/main/java/com/ig/minhasfinancas/controllers/UsuarioController.java
+++ src/main/java/com/ig/minhasfinancas/controllers/UsuarioController.java
@@ -1,13 +1,39 @@
 package com.ig.minhasfinancas.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ig.minhasfinancas.dto.UsuarioDTO;
+import com.ig.minhasfinancas.entities.Usuario;
+import com.ig.minhasfinancas.exceptions.RegraNegocioException;
+import com.ig.minhasfinancas.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "hello world!";
+    private final UsuarioService service;
+
+    public UsuarioController(UsuarioService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+
+        Usuario usuario = Usuario.builder()
+                .nome(dto.getNome())
+                .email(dto.getEmail())
+                .senha(dto.getSenha())
+                .build();
+
+        try {
+            Usuario usuarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity<>(usuarioSalvo, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 }
